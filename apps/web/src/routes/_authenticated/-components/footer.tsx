3eeaeeb057fb87155c0f28ef8ac91450274c1b3d
Udoh Jeremiah <98605632+udohjeremiah@users.noreply.github.com>
2026-02-11 05:34:41 +0100

    feat: add footer layout component
    This adds the footer component on the authenticated layout shown on all authenticated pages.

--- apps/web/src/routes/_authenticated/-components/footer.tsx
+++ apps/web/src/routes/_authenticated/-components/footer.tsx
@@ -1,5 +1,23 @@
+import AppLink from "@/components/app-link";
+
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className="border-t bg-background p-4 sm:px-8 md:px-16"></footer>
+    <footer className="flex items-center justify-center border-t p-4 sm:px-8 md:px-16">
+      <div className="flex flex-wrap items-center justify-center gap-1">
+        <p className="text-xs text-muted-foreground">
+          &copy; 2026 - {currentYear} Grepedia. All rights reserved
+        </p>
+        <span className="text-xs text-muted-foreground">•</span>
+        <AppLink className="text-xs" to="/terms-of-service">
+          Terms of Service
+        </AppLink>
+        <span className="text-xs text-muted-foreground">•</span>
+        <AppLink className="text-xs" to="/privacy-policy">
+          Privacy Policy
+        </AppLink>
+      </div>
+    </footer>
   );
 }
