d6b967d5bf44aa8b113192b8d1af43888bb18a94
Udoh Jeremiah <98605632+udohjeremiah@users.noreply.github.com>
2026-03-28 23:04:28 +0100

    fix: resolve server-side .env variables
    This resolves server-side environment variables for SSR.

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -38,8 +38,5 @@ export const env = createEnv({
    * What object holds the environment variables at runtime. This is usually
    * `process.env` or `import.meta.env`.
    */
-  runtimeEnv: {
-    ...import.meta.env,
-    ...process.env,
-  },
+  runtimeEnv: globalThis.window === undefined ? process.env : import.meta.env,
 });
