0d0131de3571191fda59ad9083442e989e787a5a
Udoh Jeremiah <98605632+udohjeremiah@users.noreply.github.com>
2026-04-05 18:31:08 +0100

    fix(web): centralize tools count
    This centralizes the tools count.

--- apps/web/src/routes/(search)/-components/tools-count.tsx
+++ apps/web/src/routes/(search)/-components/tools-count.tsx
@@ -6,7 +6,7 @@ export default function ToolsCount() {
   const { data: toolsCount } = useToolsCount();
 
   return (
-    <hgroup className="flex flex-col items-center">
+    <hgroup className="flex flex-col items-center text-center">
       <h3 className="text-xs text-muted-foreground">Tools in Grepedia</h3>
       <p className="text-3xl font-semibold">
         {formatCompactNumber(toolsCount)}
